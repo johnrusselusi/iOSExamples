40015ae32cea8a88060c4b8ac09192127e8cb1cf
John Russel Usi <j.usi@klab.com>
2014-09-30 16:05:04 +0800

    Modified Homepwner

---
+++ Homepwner/BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by John Russel Usi on 9/30/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
