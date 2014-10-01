adf0bb47f366e8e137f698c491b860607bd1b6a3
John Russel Usi <j.usi@klab.com>
2014-10-01 10:13:19 +0800

    Finished Homepwner

---
+++ Homepwner/Homepwner/BNRImageStore.h
@@ -0,0 +1,13 @@
+//
+//  BNRImageStore.h
+//  Homepwner
+//
+//  Created by John Russel Usi on 10/1/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRImageStore : NSObject
+
+@end
---
+++ Homepwner/Homepwner/BNRImageStore.m
@@ -0,0 +1,13 @@
+//
+//  BNRImageStore.m
+//  Homepwner
+//
+//  Created by John Russel Usi on 10/1/14.
+//  Copyright (c) 2014 Klab Cyscorpions. All rights reserved.
+//
+
+#import "BNRImageStore.h"
+
+@implementation BNRImageStore
+
+@end
